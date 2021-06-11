3dad0577a06d2cee0bc7e362504f79dd4567984b
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-11 11:38:30 -0500

    adds mapbox-geocoder-utils

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -8,8 +8,13 @@ var map = new mapboxgl.Map({
 	zoom: 12
 });
 
+let marker = setMarker([-98.4936, 29.4241])
+
 let geocoder = setGeoCoder()
 addGeocoderToMap(geocoder)
+addGeocoderEvent(geocoder)
+addMapEvent(marker)
+
 
 function setGeoCoder() {
 	return new MapboxGeocoder({
@@ -23,3 +28,28 @@ function addGeocoderToMap(geocoder) {
 	map.addControl(geocoder)
 }
 
+//creates the marker
+function setMarker(point) {
+	return new mapboxgl.Marker().setLngLat(point)
+								.addTo(map);
+
+}
+
+//changes pin to wherever geocoder searches
+function addGeocoderEvent(geocoder) {
+	geocoder.on("result", function (event) {
+		marker.setLngLat(event.result.geometry.coordinates)
+
+		setPopup(event.result.result_placename)
+	})
+}
+
+//adds event to map that changes location of mark
+//based on where the user clicks
+function addMapEvent() {
+	map.on('click', function (event) {
+		console.log(event.lngLat)
+		marker.setLngLat(event.lngLat)
+			  .addTo(map)
+	})
+}
